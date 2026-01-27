b52be4b31e6a16f55059ca410aa0ffe3139f0ca0
Wenyu <232058504+vvenyu@users.noreply.github.com>
2026-01-27 21:59:30 +0100

    finished

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  if (num < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < num; i++) {
+    string += word;
+  }
+  return string;
 };
+
+console.log(repeatString);
 
 // Do not edit below this line
 module.exports = repeatString;
