e88d661d8da3005d4ac066182ab95f41d978550e
wenyu <232058504+vvenyu@users.noreply.github.com>
2026-02-15 11:37:46 +0100

    finished ex

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  if(a === 0){
+    return 1;
+  } else {
+    let b = 1;
+    for(let i = 1; i <= a; i++){
+      b *= i;
+    }
+    return b;
+  };
+  }
 
 // Do not edit below this line
 module.exports = {
